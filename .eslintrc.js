6b8302c839cffbd99fcf2de1d43bf36dc9ac006e
ikuya-461 <gaoukishi461@gmail.com>
2020-08-18 11:33:37 +0900

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true,
+  },
+  parserOptions: {
+    parser: 'babel-eslint',
+  },
+  extends: [
+    '@nuxtjs',
+    'plugin:nuxt/recommended',
+    'plugin:vue/recommended',
+    'prettier',
+    'prettier/vue',
+  ],
+  plugins: ['prettier'],
+  // add your custom rules here
+  rules: {
+    'no-console': 0, // console.log を置いても怒られないように
+  },
+}
